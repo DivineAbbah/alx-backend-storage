4b36bd98c6f34f4826c181196acc4bc8eba16ab1
Divine Abbah <abbahdivine@gmail.com>
2023-12-16 23:50:28 +0000

    create user table

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- create the users table with the fields:
+-- id, email, name, country(enum: US, CO, and TN)
+CREATE TABLE IF NOT EXISTS users (
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+)
